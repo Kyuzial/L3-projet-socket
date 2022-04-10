6b2da88f034d51fb8b5473b01467b95bf1e81b29
Kyuzial <kyuzial@protonmail.com>
2022-04-10 20:31:51 +0200

    Fix formating

--- src/client/client.c
+++ src/client/client.c
@@ -7,7 +7,8 @@
 
 #define PORT 8080
 
-int tcpMode(int argc, char *argv[]) {
+int tcpMode(int argc, char *argv[])
+{
 	struct sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
@@ -15,17 +16,20 @@ int tcpMode(int argc, char *argv[]) {
 	char buffer[2048] = {0};
 	int sockfd;
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		fprintf(stderr, "Couldn't create socket\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+	{
 		fprintf(stderr, "Invalid address/ Address not supported \n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
 		fprintf(stderr, "Connection Failed \n");
 		exit(EXIT_FAILURE);
 	}
@@ -35,16 +39,20 @@ int tcpMode(int argc, char *argv[]) {
 	send(sockfd, message, strlen(message), 0);
 	printf("Hello message sent\n");
 
-	if (recv(sockfd, buffer, 2048, 0) < 1) {
+	if (recv(sockfd, buffer, 2048, 0) < 1)
+	{
 		fprintf(stderr, "recv failed\n");
 		return 1;
-	} else {
+	}
+	else
+	{
 		printf("%s\n", buffer);
 	}
 	return 1;
 }
 
-int udpMode(int argc, char *argv[]) {
+int udpMode(int argc, char *argv[])
+{
 
 	struct sockaddr_in serv_addr;
 
@@ -52,7 +60,8 @@ int udpMode(int argc, char *argv[]) {
 	int sockfd;
 
 	// Creating socket file descriptor
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
@@ -68,11 +77,11 @@ int udpMode(int argc, char *argv[]) {
 	char *message = "Client : hello ";
 
 	sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM,
-			(const struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		   (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
 	printf("Hello message sent.\n");
 
 	n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL,
-				(struct sockaddr *)&serv_addr, &len);
+				 (struct sockaddr *)&serv_addr, &len);
 	buffer[n] = '\0';
 	printf("Server : %s\n", buffer);
 
@@ -80,12 +89,16 @@ int udpMode(int argc, char *argv[]) {
 	return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	if (!strcmp(argv[1], "TCP")) {
+	if (!strcmp(argv[1], "TCP"))
+	{
 		printf("salut\n");
 		tcpMode(argc, argv);
-	} else if (!strcmp(argv[1], "UDP")) {
+	}
+	else if (!strcmp(argv[1], "UDP"))
+	{
 		udpMode(argc, argv);
 	}
 
--- src/server/server.c
+++ src/server/server.c
@@ -7,18 +7,18 @@
 
 #define PORT 8080
 
-int tcpMode(int argc, char *argv[]) {
+int tcpMode()
+{
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
     int addrlen = sizeof(address);
     int opt = 1;
     int new_socket;
     char buffer[2048] = {0};
     int sockfd = 0;
-
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -40,68 +40,75 @@ int tcpMode(int argc, char *argv[]) {
 
     listen(sockfd, 3);
 
-    if ((new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0 )
+    if ((new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
     {
         fprintf(stderr, "accept failed\n");
         exit(EXIT_FAILURE);
     }
 
-    char* message = "Server: hello";
+    char *message = "Server: hello";
     while (recv(new_socket, buffer, 2048, 0) > 0)
     {
-        printf("%s\n",buffer );
+        printf("%s\n", buffer);
         send(new_socket, message, strlen(message), 0);
         printf("Message sent to client\n");
     }
-    return 0;}
-
-int udpMode(int argc, char *argv[]) {
-
-	int sockfd;
-	char buffer[2048];
-	char *hello = "Hello from server";
-	struct sockaddr_in serveur, client;
-
-	// Creating socket file descriptor
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
-
-	memset(&serveur, 0, sizeof(serveur));
-	memset(&client, 0, sizeof(client));
-	// Filling server information
-	serveur.sin_family = AF_INET; // IPv4
-	serveur.sin_addr.s_addr = INADDR_ANY;
-	serveur.sin_port = htons(PORT);
-
-	// Bind the socket with the server address
-	if (bind(sockfd, (const struct sockaddr *)&serveur, sizeof(serveur)) < 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-
-	int len, n;
-
-	len = sizeof(client); // len is value/resuslt
-
-	n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL,
-				(struct sockaddr *)&client, &len);
-	buffer[n] = '\0';
-	printf("%s\n", buffer);
-	sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
-			(const struct sockaddr *)&client, len);
-	printf("Hello message sent.\n");
-
-	return 0;
+    return 0;
 }
 
-int main(int argc, char *argv[]) {
-	if (!strcmp(argv[1], "TCP")) {
-		tcpMode(argc, argv);
-	} else if (!strcmp(argv[1], "UDP")) {
-		udpMode(argc, argv);
-	}
+int udpMode()
+{
 
-	return 0;
+    int sockfd;
+    char buffer[2048];
+    char *hello = "Hello from server";
+    struct sockaddr_in serveur, client;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serveur, 0, sizeof(serveur));
+    memset(&client, 0, sizeof(client));
+    // Filling server information
+    serveur.sin_family = AF_INET; // IPv4
+    serveur.sin_addr.s_addr = INADDR_ANY;
+    serveur.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&serveur, sizeof(serveur)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int len, n;
+
+    len = sizeof(client); // len is value/resuslt
+
+    n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL,
+                 (struct sockaddr *)&client, &len);
+    buffer[n] = '\0';
+    printf("%s\n", buffer);
+    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
+           (const struct sockaddr *)&client, len);
+    printf("Hello message sent.\n");
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (!strcmp(argv[1], "TCP"))
+    {
+        tcpMode(argc, argv);
+    }
+    else if (!strcmp(argv[1], "UDP"))
+    {
+        udpMode(argc, argv);
+    }
+    return 0;
 }
